432b7924386442c2f89fd352a7953782909d4e0c
ntien <ntien.se03@gmail.com>
2024-07-10 09:29:15 +0700

    Refactor

--- identity-service/src/main/java/com/tien/identity/dto/request/ExchangeTokenRequest.java
+++ identity-service/src/main/java/com/tien/identity/dto/request/ExchangeTokenRequest.java
@@ -1,15 +1,9 @@
 package com.tien.identity.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.tien.identity.validator.DobConstraint;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
