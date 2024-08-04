f0fcbb4a256f5efd2c8335a2d463c60193244c3d
ntien <ntien.se03@gmail.com>
2024-08-04 22:18:07 +0700

    config prometheus.yml + add payment for order service + delete stripe

--- payment-service/src/main/java/com/tien/payment/controller/PayPalController.java
+++ payment-service/src/main/java/com/tien/payment/controller/PayPalController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class PayPalController {
 
-      final PayPalService paypalService;
+      PayPalService paypalService;
 
       @PostMapping("/create")
       public ApiResponse<String> createPayment(
