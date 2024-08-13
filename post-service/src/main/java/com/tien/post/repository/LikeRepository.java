89728254252e6e84e28dfb071ed8c3695e68763f
ntien <ntien.se03@gmail.com>
2024-08-13 16:41:15 +0700

    format

--- post-service/src/main/java/com/tien/post/repository/LikeRepository.java
+++ post-service/src/main/java/com/tien/post/repository/LikeRepository.java
@@ -4,8 +4,6 @@ import com.tien.post.entity.Like;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface LikeRepository extends MongoRepository<Like, String> {
 
@@ -14,7 +12,5 @@ public interface LikeRepository extends MongoRepository<Like, String> {
       boolean existsByPostIdAndUserId(String postId, String userId);
 
       void deleteByPostIdAndUserId(String postId, String userId);
-
-      List<Like> findByPostId(String postId);
 
 }
