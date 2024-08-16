47cde4d1529a83d9c730c930234cc551d7c12618
ntien <ntien.se03@gmail.com>
2024-08-16 11:21:46 +0700

    format

--- payment-service/src/main/java/com/tien/payment/repository/PayPalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tien.payment.repository;
-
-import com.tien.payment.entity.PayPal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PayPalRepository extends JpaRepository<PayPal, Long> {}
