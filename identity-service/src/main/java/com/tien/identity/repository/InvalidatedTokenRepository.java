588486be0b816d533d7c7b9206dd16b3fd75b0b1
ntien <ntien.se03@gmail.com>
2024-09-03 19:38:35 +0700

    delete identity-service & profile-service

--- identity-service/src/main/java/com/tien/identity/repository/InvalidatedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tien.identity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tien.identity.entity.InvalidatedToken;
-
-@Repository
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {}
--- identity-service/src/main/java/com/tien/identity/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tien.identity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tien.identity.entity.Permission;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, String> {}
