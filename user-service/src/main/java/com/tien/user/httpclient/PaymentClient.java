7452127a70273e23d73ef73dfee3f0e22e221bb0
ntien <ntien.se03@gmail.com>
2024-10-20 10:00:11 +0700

    refactor

--- user-service/src/main/java/com/tien/user/httpclient/PaymentClient.java
+++ user-service/src/main/java/com/tien/user/httpclient/PaymentClient.java
@@ -5,6 +5,8 @@ import com.tien.user.dto.ApiResponse;
 import com.tien.user.dto.request.StripeSubscriptionRequest;
 import com.tien.user.dto.request.SubscriptionCancelRecord;
 import com.tien.user.dto.response.StripeSubscriptionResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +14,22 @@ import org.springframework.web.bind.annotation.*;
         configuration = {AuthenticationRequestInterceptor.class})
 public interface PaymentClient {
 
+      @CircuitBreaker(name = "createSubscription", fallbackMethod = "createSubscriptionFallback")
+      @Retry(name = "createSubscription")
       @PostMapping("/stripe/customer/subscription")
       ApiResponse<StripeSubscriptionResponse> createSubscription(@RequestBody StripeSubscriptionRequest request);
 
+      @CircuitBreaker(name = "cancelSubscription", fallbackMethod = "cancelSubscriptionFallback")
+      @Retry(name = "cancelSubscription")
       @DeleteMapping("/stripe/subscription/{id}")
       ApiResponse<SubscriptionCancelRecord> cancelSubscription(@PathVariable("id") String id);
 
-      @GetMapping("/stripe/subscription/{id}")
-      ApiResponse<StripeSubscriptionResponse> retrieveSubscriptionDetails(@PathVariable("id") String id);
+      default ApiResponse<StripeSubscriptionResponse> createSubscriptionFallback(StripeSubscriptionRequest request, Throwable throwable) {
+            throw new RuntimeException();
+      }
+
+      default ApiResponse<SubscriptionCancelRecord> cancelSubscriptionFallback(String id, Throwable throwable) {
+            throw new RuntimeException();
+      }
 
 }
