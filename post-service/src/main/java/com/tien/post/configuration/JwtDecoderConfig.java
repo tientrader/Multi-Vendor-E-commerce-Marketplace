f3329a8602f9c12a0594529fcebc96928875c298
ntien <truongnhattien.business@gmail.com>
2025-01-07 22:12:26 +0700

    refactor

--- post-service/src/main/java/com/tien/post/configuration/JwtDecoderConfig.java
+++ post-service/src/main/java/com/tien/post/configuration/JwtDecoderConfig.java
@@ -1,5 +1,6 @@
 package com.tien.post.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -8,9 +9,12 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 @Configuration
 public class JwtDecoderConfig {
 
+      @Value("${keycloak.url}")
+      private String keycloakUrl;
+
       @Bean
       public JwtDecoder jwtDecoder() {
-            return NimbusJwtDecoder.withJwkSetUri("http://localhost:8180/realms/tienproapp/protocol/openid-connect/certs").build();
+            return NimbusJwtDecoder.withJwkSetUri(keycloakUrl).build();
       }
 
 }
--- shop-service/src/main/java/com/tien/shop/configuration/JwtDecoderConfig.java
+++ shop-service/src/main/java/com/tien/shop/configuration/JwtDecoderConfig.java
@@ -1,5 +1,6 @@
 package com.tien.shop.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -8,9 +9,12 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 @Configuration
 public class JwtDecoderConfig {
 
+      @Value("${keycloak.url}")
+      private String keycloakUrl;
+
       @Bean
       public JwtDecoder jwtDecoder() {
-            return NimbusJwtDecoder.withJwkSetUri("http://localhost:8180/realms/tienproapp/protocol/openid-connect/certs").build();
+            return NimbusJwtDecoder.withJwkSetUri(keycloakUrl).build();
       }
 
 }
