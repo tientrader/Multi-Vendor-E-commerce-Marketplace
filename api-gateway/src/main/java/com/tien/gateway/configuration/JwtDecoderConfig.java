54abc3a4f3d654b989b0f8a81ca5aab1725b5cb5
ntien <truongnhattien.business@gmail.com>
2025-01-07 12:40:28 +0700

    refactor

--- api-gateway/src/main/java/com/tien/gateway/configuration/JwtDecoderConfig.java
+++ api-gateway/src/main/java/com/tien/gateway/configuration/JwtDecoderConfig.java
@@ -1,5 +1,6 @@
 package com.tien.gateway.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
@@ -8,10 +9,12 @@ import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 @Configuration
 public class JwtDecoderConfig {
 
+      @Value("${keycloak.url}")
+      private String keycloakUrl;
+
       @Bean
       public ReactiveJwtDecoder jwtDecoder() {
-            return NimbusReactiveJwtDecoder.withJwkSetUri
-                    ("http://localhost:8180/realms/tienproapp/protocol/openid-connect/certs").build();
+            return NimbusReactiveJwtDecoder.withJwkSetUri(keycloakUrl).build();
       }
 
 }
